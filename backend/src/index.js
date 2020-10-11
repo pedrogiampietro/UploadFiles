c7be655af62ea2e7b31b488fa8a9dcaad1c703a5
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-10-11 20:36:12 -0300

    save imgs to database.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect(
+	'mongodb+srv://root:899681ph@cluster0.v083q.gcp.mongodb.net/upload?retryWrites=true&w=majority',
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}
+)
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
