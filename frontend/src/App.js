eaca119d14864c1fa9c581ee3bd66fafc2a09312
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-10-12 18:10:41 -0300

    fixed bug progress changes.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,118 +1,121 @@
-import React, { Component } from 'react'
-import { uniqueId } from 'lodash'
-import GlobalStyle from './styles/global'
-import filesize from 'filesize'
+import React, { Component } from "react";
+import { uniqueId } from "lodash";
+import filesize from "filesize";
 
-import api from './services/api'
+import api from "./services/api";
 
-import { Container, Content } from './styles'
+import GlobalStyle from "./styles/global";
+import { Container, Content } from "./styles";
 
-import Upload from './components/Upload'
-import FileList from './components/FileList'
+import Upload from "./components/Upload";
+import FileList from "./components/FileList";
 
 class App extends Component {
-	state = {
-		uploadedFiles: [],
-	}
+  state = {
+    uploadedFiles: []
+  };
 
-	async componentDidMount() {
-		const response = await api.get('posts')
-		this.setState({
-			uploadedFiles: response.data.map(file => ({
-				id: file._id,
-				name: file.name,
-				readableSize: filesize(file.size),
-				preview:file.url,
-				uploaded: true,
-				url: file.url
-			}))
-		})
-	}
+  async componentDidMount() {
+    const response = await api.get("posts");
 
-	handleUpload = (files) => {
-		const uploadedFiles = files.map((file) => ({
-			file,
-			id: uniqueId(),
-			name: file.name,
-			readableSize: filesize(file.size),
-			preview: URL.createObjectURL(file),
-			progress: 0,
-			uploaded: false,
-			error: false,
-			url: null,
-		}))
+    this.setState({
+      uploadedFiles: response.data.map(file => ({
+        id: file._id,
+        name: file.name,
+        readableSize: filesize(file.size),
+        preview: file.url,
+        uploaded: true,
+        url: file.url
+      }))
+    });
+  }
 
-		this.setState({
-			uploadedFiles: this.state.uploadedFiles.concat(uploadedFiles),
-		})
+  handleUpload = files => {
+    const uploadedFiles = files.map(file => ({
+      file,
+      id: uniqueId(),
+      name: file.name,
+      readableSize: filesize(file.size),
+      preview: URL.createObjectURL(file),
+      progress: 0,
+      uploaded: false,
+      error: false,
+      url: null
+    }));
 
-		uploadedFiles.forEach(this.processUpload)
-	}
+    this.setState({
+      uploadedFiles: this.state.uploadedFiles.concat(uploadedFiles)
+    });
 
-	updateFile = (id, data) => {
-		this.setState({
-			uploadedFiles: this.state.uploadedFiles.map((uploadedFile) => {
-				return id === uploadedFile.id
-					? { ...uploadedFile, ...data }
-					: uploadedFile
-			}),
-		})
-	}
+    uploadedFiles.forEach(this.processUpload);
+  };
 
-	processUpload = (uploadedFile) => {
-		const data = new FormData()
+  updateFile = (id, data) => {
+    this.setState({
+      uploadedFiles: this.state.uploadedFiles.map(uploadedFile => {
+        return id === uploadedFile.id
+          ? { ...uploadedFile, ...data }
+          : uploadedFile;
+      })
+    });
+  };
 
-		data.append('file', uploadedFile.file, uploadedFile.name)
+  processUpload = uploadedFile => {
+    const data = new FormData();
 
-		api
-			.post('posts', data, {
-				onUploadProgress: (e) => {
-					const progress = parseInt(Math.round((e.loaded * 100) / e.total))
+    data.append("file", uploadedFile.file, uploadedFile.name);
 
-					this.updateFile(uploadedFile.id, {
-						progress,
-					})
-				},
-			})
-			.then((response) => {
-				this.updateFile(uploadedFile.id, {
-					uploaded: true,
-					id: response.data._id,
-					url: response.data.url,
-				})
-			})
-			.catch(() => {
-				this.updateFile(uploadedFile.id, {
-					error: true,
-				})
-			})
-	}
+    api
+      .post("posts", data, {
+        onUploadProgress: e => {
+          const progress = parseInt(Math.round((e.loaded * 100) / e.total));
 
-	handleDelete = async (id) => {
-		await api.delete(`posts/${id}`)
+          this.updateFile(uploadedFile.id, {
+            progress
+          });
+        }
+      })
+      .then(response => {
+        this.updateFile(uploadedFile.id, {
+          uploaded: true,
+          id: response.data._id,
+          url: response.data.url
+        });
+      })
+      .catch(() => {
+        this.updateFile(uploadedFile.id, {
+          error: true
+        });
+      });
+  };
 
-		this.setState({
-			uploadedFiles: this.state.uploadedFiles.filter((file) => file.id !== id),
-		})
-	}
+  handleDelete = async id => {
+    await api.delete(`posts/${id}`);
 
-	componentWillUpdate() {
-		this.state.uploadedFiles.forEach(file => URL.revokeObjectURL(file.preview))
-	}
-	render() {
-		const { uploadedFiles } = this.state
-		return (
-			<Container>
-				<Content>
-					<Upload onUpload={this.handleUpload} />
-					{!!uploadedFiles.length && (
-						<FileList files={uploadedFiles} onDelete={this.handleDelete} />
-					)}
-				</Content>
-				<GlobalStyle />
-			</Container>
-		)
-	}
+    this.setState({
+      uploadedFiles: this.state.uploadedFiles.filter(file => file.id !== id)
+    });
+  };
+
+  componentWillUnmount() {
+    this.state.uploadedFiles.forEach(file => URL.revokeObjectURL(file.preview));
+  }
+
+  render() {
+    const { uploadedFiles } = this.state;
+
+    return (
+      <Container>
+        <Content>
+          <Upload onUpload={this.handleUpload} />
+          {!!uploadedFiles.length && (
+            <FileList files={uploadedFiles} onDelete={this.handleDelete} />
+          )}
+        </Content>
+        <GlobalStyle />
+      </Container>
+    );
+  }
 }
 
-export default App
+export default App;
--- frontend/src/styles/global.js
+++ frontend/src/styles/global.js
@@ -1,24 +1,24 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from "styled-components";
 
-import 'react-circular-progressbar/dist/styles.css'
+import "react-circular-progressbar/dist/styles.css";
 
 export default createGlobalStyle`
-    * {
-         margin: 0;
-         padding: 0;
-         outline: 0;
-         box-sizing: border-box;
-    }
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
 
-    body {
-        font-family: Arial, Helvetica, sans-serif;
-        font-size: 14px;
-        background: #7159c1;
-        text-rendering: optimizeLegibility;
-        -webkit-font-smoothing: antialiased;
-    }
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 14px;
+    background: #7159c1;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+  }
 
-    html, body, #root {
-        height: 100%;
-    }
-`
+  html, body, #root {
+    height: 100%;
+  }
+`;
